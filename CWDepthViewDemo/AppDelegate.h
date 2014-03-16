58847b93812bdc45115217fae33ee70bcb97371d
Cezary Wojcik <cezarywojcik92@gmail.com>
2014-03-15 20:44:25 -0700

    Initial Commit

---
+++ CWDepthViewDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CWDepthViewDemo
+//
+//  Created by Cezary Wojcik on 3/15/14.
+//  Copyright (c) 2014 Cezary Wojcik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
